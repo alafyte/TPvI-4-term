64e7a8a547ab0bee4338ca2ea27ae47120335cc5
alafyte <xleb.xlebnyj@mail.ru>
2023-05-19 17:58:00 +0300

    Лабораторная работа №9

--- Lab09/SQLQuery1.sql
+++ Lab09/SQLQuery1.sql
@@ -5,5 +5,5 @@
 	User_Password nvarchar(50) NOT NULL,
 	User_Role nvarchar(10) NOT NULL
 )
-insert into Users ([ID], [User_Login], [User_Password], [User_Role]) VALUES (1, N'admin', N'⌡⤯坺ꞥ襃๊聊쌟', N'admin')
-insert into Users ([ID], [User_Login], [User_Password], [User_Role]) VALUES (18, N'user1', N'줤廡꽒糄嬢繵鴟', N'user')
+insert into Users ([User_Login], [User_Password], [User_Role]) VALUES (N'admin', N'⌡⤯坺ꞥ襃๊聊쌟', N'admin')
+insert into Users ([User_Login], [User_Password], [User_Role]) VALUES (N'user1', N'줤廡꽒糄嬢繵鴟', N'user')
