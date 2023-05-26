0c076a7314d91d6af09f987466b37f7c90b048c0
alafyte <xleb.xlebnyj@mail.ru>
2023-05-26 20:51:15 +0300

    Лабораторная работа №10

---
+++ Lab10/SQLQuery1.sql
@@ -0,0 +1,9 @@
+USE Lab09
+CREATE TABLE Timetable1 (
+	ClassId int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	ClassName nvarchar (150) NOT NULL,
+	ClassDay nvarchar (20) NOT NULL,
+	ClassHours nvarchar (15) NOT NULL,
+)
+
+
